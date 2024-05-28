cdc7786d471628bbf0895bcf2e773f51e13e25bc
Almuheetu Shihab <almuheetu@gmail.com>
2024-05-28 12:26:26 +0600

    some problem issu fixing

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,26 @@
+val result = 80
+val resultOne:Int = 95
 
+val totalResult = result + resultOne
 fun main() {
 
-    println("Hello my ")
+  println("my result: $totalResult")
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
