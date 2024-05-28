9fbd276f014eac20cc6f7fd157dd66af48513af9
Almuheetu Shihab <almuheetu@gmail.com>
2024-05-28 12:53:34 +0600

    infix function practice started

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,4 @@
+// Explicit and Implicit
 fun printHello(name: String?): Int {
   val a: Int = 89
   if (name != null)
@@ -7,10 +8,19 @@ fun printHello(name: String?): Int {
 
   return 58
 }
+
+
+// infix function
+infix fun Int.entu(a: Int): Int {
+  return this * a
+}
+
+
 fun main() {
 
   println("${printHello("Shihab")}")
 }
+
 
 
 
